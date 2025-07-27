2c67f4d2d8f7ca57f4450b551dc54894f8de602d
Williamparededl1 <wparedesl1995@gmail.com>
2025-07-26 23:02:06 -0500

    update functions

--- src/clean-code/03-functions.ts
+++ src/clean-code/03-functions.ts
@@ -1,34 +1,43 @@
 (() => {
 
     // función para obtener información de una película por Id
-    function getAllMovies( movieId: string ) {
+    function getMovieById( movieId: string ) {
         console.log({ movieId });
     }
 
     // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
-    function getAllMovieActors( id: string ) {
-        console.log({ id });
+    function getMovieActorsById( movieId: string ) {
+        console.log({ movieId });
     }
 
     // funcion para obtener el bio del actor por el id
-    function getUsuario( ActorId: string ) {
+    function getActorById( ActorId: string ) {
         console.log({ ActorId });
     }
     
+    interface Movie {
+        cast:        string[];      
+        description: string;
+        rating:      number;
+        title:       string; 
+    }
     // Crear una película
-    function movie(title: string, description: string, rating: number, cast: string[] ) {
+    function createMovie({title, description, rating, cast}: Movie): void {
         console.log({ title, description, rating, cast });
     }
 
     // Crea un nuevo actor
-    function createActorIfActorNotExists( fullName: string, birthdate: Date ): boolean {
-        
-        // tarea asincrona para verificar nombre
-        // ..
-        // ..
-        if ( fullName === 'fernando' ) return false;
 
-        console.log('Crear actor');
+    function checkFullName( fullName: string ): boolean {
+    console.log('Verificando nombre', fullName);
+    return true;
+    }
+    function createActor( fullName: string, birthdate: Date ): boolean {
+        
+    
+        if ( checkFullName(fullName) ) return false;
+
+        console.log('Crear actor',birthdate);
         return true;        
 
     }
